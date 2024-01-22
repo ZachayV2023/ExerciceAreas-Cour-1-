3e22b18d70e67d0f92671ff773e7fd369818d51b
Zachary Vanier <2037408@cegepmontpetit.ca>
2024-01-22 12:58:38 -0500

    Initialize project

---
+++ AreasTest/Models/Chat.cs
@@ -0,0 +1,9 @@
+﻿namespace AreasTest.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
